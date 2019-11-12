05f259902950c1ac604ab9d86b8682ff97e604ef
VS Chandra Mourya <30852815+VSMourya@users.noreply.github.com>
2019-11-12 16:39:40 +0530

    Update laserMirrorBoat ( InterviewBit ).py

--- laserMirrorBoat ( InterviewBit ).py
+++ laserMirrorBoat ( InterviewBit ).py
@@ -3,60 +3,64 @@
 #This code can be made even shorter but the primary focus remains the same
 #The code should be readable and be more simple to understand :)
 
-def isOutOfBounds(matrix,row,col):
 
-    return row < 0 or row > len(matrix)-1 or col < 0 or col > len(matrix)-1
+def isOutOfBounds(row,col,matrix):
 
-def checkPossibility(matrix,row,col):
-
-    possibilityDown = True
-    possibilityRight = True
-
-    while not isOutOfBounds(matrix,row,col):
-        if matrix[row][col] == 1:
-            possibilityRight = False
-            break
-        col += 1
-
-    while not isOutOfBounds(matrix,row,col):
-        if matrix[row][col] == 1:
-            possibilityDown = False
-            break
-        row += 1
+    return row<0 or row>len(matrix)-1 or col<0 or col>len(matrix[row])-1
 
 
-    if possibilityDown and possibilityRight:
-        return True
-    else:
-        return False
+def checkPossibility(row,col,matrix):
+
+    down = True
+    right = True
+
+    i = row
+    while i < len(matrix):
+        if matrix[i][col] == 1:
+            down = False
+        i+=1
+
+    j = col
+    while j < len(matrix[row]):
+        if matrix[row][j] == 1:
+            right = False
+        j+=1
+
+    return down and right
+
 
 def getCount(matrix):
 
-    row,col = 0,len(matrix)-1
     count = 0
+    row,col = 0,len(matrix)-1
 
-    while not isOutOfBounds(matrix,row,col):
+    while not isOutOfBounds(row,col,matrix):
 
         if col != 0:
             if matrix[row][col] == 0:
-                if checkPossibility(matrix,row,col):
-                    count+=1
+                if checkPossibility(row,col,matrix):
+                    count +=1
                     col-=1
                 else:
                     col-=1
             else:
-                row+=1
-                col = len(matrix)-1
-        else:
-            if matrix[row][col] == 1:
                 row += 1
                 col = len(matrix) - 1
+        else:
+            if matrix[row][col] == 0:
+                if checkPossibility(row,col,matrix):
+                    count += 1
+                    row += 1
+                    col = len(matrix) - 1
+                else:
+                    row += 1
+                    col = len(matrix) - 1
             else:
-                count+=1
                 row += 1
                 col = len(matrix) - 1
 
     return count
+
 
 matrix = [[1,0,1],
           [1,0,0],
